9f2c3a05f4ec6a3ce1ad7ea5fb0567b9f92132bd
현태 최 <ht3064@naver.com>
2024-09-02 00:58:19 +0900

    feat: 페이징 및 검색 API 구현

--- src/main/java/study/querydsl/domain/member/api/MemberController.java
+++ src/main/java/study/querydsl/domain/member/api/MemberController.java
@@ -1,6 +1,8 @@
 package study.querydsl.domain.member.api;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import study.querydsl.domain.member.application.MemberService;
@@ -15,8 +17,19 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @Deprecated
     @GetMapping("/v1/members")
     public List<MemberTeamDto> memberSearchV1(MemberSearchCondition condition) {
         return memberService.searchMemberV1(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public List<MemberTeamDto> memberSearchV2(MemberSearchCondition condition) {
+        return memberService.searchMemberV2(condition);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> memberSearchV3(MemberSearchCondition condition, Pageable pageable) {
+        return memberService.searchMemberV3(condition, pageable);
     }
 }
--- src/main/java/study/querydsl/domain/member/application/MemberService.java
+++ src/main/java/study/querydsl/domain/member/application/MemberService.java
@@ -1,9 +1,12 @@
 package study.querydsl.domain.member.application;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.domain.member.dao.MemberJpaRepository;
+import study.querydsl.domain.member.dao.MemberRepository;
 import study.querydsl.domain.member.dto.request.MemberSearchCondition;
 import study.querydsl.domain.member.dto.response.MemberTeamDto;
 
@@ -14,9 +17,21 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MemberService {
 
+    @Deprecated
     private final MemberJpaRepository memberJpaRepository;
 
+    private final MemberRepository memberRepository;
+
+    @Deprecated
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.searchByWhereParam(condition);
+    }
+
+    public List<MemberTeamDto> searchMemberV2(MemberSearchCondition condition) {
+        return memberRepository.search(condition);
+    }
+
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPage(condition, pageable);
     }
 }
